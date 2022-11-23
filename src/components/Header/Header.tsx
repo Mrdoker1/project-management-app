c7bc83c5de224b90ea319da8ffaf9f59813b0768
Vlad <makrak.vladislav@yandex.ru>
2022-11-23 21:33:13 +0300

    feat: add sticky header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,16 @@
 import { Menu, Center, Header, Container, Group, Burger } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import React, { memo, useEffect, useState } from 'react';
+import { useDisclosure, useWindowEvent } from '@mantine/hooks';
+import React, { memo, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import cl from './Header.module.css';
-import { useStyles } from './HeaderStyles';
 import logo from '../../assets/logo.svg';
 import DrawerComponent from './Drawer/Drawer';
 import LoginButtons from './LoginButtons/LoginButtons';
 import MenuComponent from './Menu/Menu';
 import i18n from 'i18n';
-
-interface HeaderActionProps {
+import { IconChevronDown } from '@tabler/icons';
+import { useTranslation } from 'react-i18next';
+interface IHeaderProps {
   links: {
     link: string;
     label: string;
@@ -18,18 +18,25 @@ interface HeaderActionProps {
   }[];
 }
 
-const HeaderAction = memo(({ links }: HeaderActionProps) => {
-  const { classes } = useStyles();
+const HeaderAction = memo(({ links }: IHeaderProps) => {
+  const { t } = useTranslation();
   const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
-  const [language, setLanguage] = useState('English');
+  const [sticky, setSticky] = useState(false);
+  const language = localStorage.getItem('i18nextLng') || 'English';
 
-  useEffect(() => {
+  const stickyHeader = sticky ? cl.sticky : '';
+  const trackScroll = () => {
+    setSticky(window.scrollY >= 120);
+  };
+  useWindowEvent('scroll', trackScroll);
+
+  const handleChangeLang = (language: string) => {
     i18n.changeLanguage(language);
-  }, [language]);
+  };
 
   const menuItems = links.map((link) => {
     const items = link.links?.map((item) => (
-      <Menu.Item key={item.link} onClick={() => setLanguage(item.label)}>
+      <Menu.Item key={item.link} onClick={() => handleChangeLang(item.label)}>
         {item.label}
       </Menu.Item>
     ));
@@ -39,8 +46,8 @@ const HeaderAction = memo(({ links }: HeaderActionProps) => {
         <Menu key={link.label} trigger="hover" exitTransitionDuration={0}>
           <Menu.Target>
             <Center>
-              <span className={classes.linkLabel}>{language}</span>
-              {/* <IconChevronDown size={12} stroke={1.5} /> */}
+              <span className={cl.linkLabel}>{language}</span>
+              <IconChevronDown size={12} stroke={1.5} />
             </Center>
           </Menu.Target>
           <Menu.Dropdown>{items}</Menu.Dropdown>
@@ -49,22 +56,22 @@ const HeaderAction = memo(({ links }: HeaderActionProps) => {
     }
 
     return (
-      <NavLink key={link.label} to={link.link} className={classes.link}>
-        {link.label}
+      <NavLink key={link.label} to={link.link} className={cl.link}>
+        {t(link.label)}
       </NavLink>
     );
   });
 
   return (
     <>
-      <Header className={classes.header} height={64}>
-        <Container size={1200} className={classes.inner}>
+      <Header className={`${cl.header} ${stickyHeader}`} height={64}>
+        <Container size={1200} className={cl.inner}>
           <Group>
             <Burger
               color="#fff"
               opened={drawerOpened}
               onClick={toggleDrawer}
-              className={classes.hiddenDesktop}
+              className={cl.hiddenDesktop}
             />
             <NavLink className={cl.logoWrapper} end to="/">
               <img src={logo} alt="Logo" />
