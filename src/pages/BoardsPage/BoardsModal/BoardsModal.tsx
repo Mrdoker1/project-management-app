0c7634d6ae25c73f0517717dc823c0f8d2c759ad
Viachaslau Kulbitski <fixrapdok@gmail.com>
2022-11-24 11:45:39 +0300

    fix: split board form

--- src/pages/BoardsPage/BoardsModal/BoardsModal.tsx
+++ src/pages/BoardsPage/BoardsModal/BoardsModal.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { setModalState, actionType } from 'store/boardsSlice';
-import { Modal, Select, TextInput, Button, Textarea, ColorInput } from '@mantine/core';
+import { Modal, Select, TextInput, Button, Textarea, ColorInput, Loader } from '@mantine/core';
 import { useAppDispatch, useAppSelector } from 'hooks/redux';
 import { useForm } from '@mantine/form';
 import cl from './BoardsModal.module.css';
@@ -26,6 +26,15 @@ const BoardsModal = () => {
 
   const [usersList, setUsers] = useState([{ value: '', label: '', key: '' }]);
   const [boardData, setBoard] = useState(defaultValues);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const form = useForm({
+    initialValues: boardData,
+    validate: {
+      name: (value) => (value.length < 2 ? 'Name must have at least 2 letters' : null),
+      description: (value) => (value.length < 2 ? 'Name must have at least 2 letters' : null),
+    },
+  });
 
   useEffect(() => {
     if (users) {
@@ -44,16 +53,76 @@ const BoardsModal = () => {
       };
       modal.type == 1 ? form.setValues(values) : form.setValues(defaultValues);
       setBoard(values);
+      setIsLoading(false);
     }
   }, [board, modal]);
 
-  const form = useForm({
-    initialValues: boardData,
-    validate: {
-      name: (value) => (value.length < 2 ? 'Name must have at least 2 letters' : null),
-      description: (value) => (value.length < 2 ? 'Name must have at least 2 letters' : null),
-    },
-  });
+  const formComponent = (
+    <form
+      className={cl.form}
+      onSubmit={form.onSubmit(async (values) => {
+        if (modal.type === 1) {
+          try {
+            await updateBoard({
+              _id: modal.board.id,
+              title: values.name,
+              owner: values.owner,
+              description: values.description,
+              color: values.color,
+              users: [],
+            }).unwrap();
+            dispatch(setModalState(false));
+          } catch (error) {
+            console.error('rejected', error);
+          }
+        } else {
+          try {
+            await createBoard({
+              title: values.name,
+              owner: values.owner,
+              description: values.description,
+              color: values.color ? values.color : 'rgb(140, 140, 140)',
+              users: [],
+            }).unwrap();
+            dispatch(setModalState(false));
+          } catch (error) {
+            console.error('rejected', error);
+          }
+        }
+      })}
+    >
+      <TextInput
+        classNames={inputClasses}
+        label="Name"
+        placeholder="Name"
+        {...form.getInputProps('name')}
+      />
+      <Select
+        searchable
+        classNames={inputClasses}
+        label="Owner"
+        placeholder="Select user"
+        data={usersList}
+        {...form.getInputProps('owner')}
+      />
+      <ColorInput
+        classNames={inputClasses}
+        label="Color"
+        format="rgb"
+        placeholder="Select color"
+        {...form.getInputProps('color')}
+      />
+      <Textarea
+        classNames={inputClasses}
+        label="Description"
+        placeholder="Description"
+        {...form.getInputProps('description')}
+      />
+      <Button className={cl.submit} type="submit" mt="sm">
+        {modal.type === 1 ? 'Save' : 'Create'}
+      </Button>
+    </form>
+  );
 
   return (
     <Modal
@@ -64,70 +133,7 @@ const BoardsModal = () => {
       }}
       title={modal.type === actionType.Edit ? 'Edit Board' : 'Create Board'}
     >
-      <form
-        className={cl.form}
-        onSubmit={form.onSubmit(async (values) => {
-          if (modal.type === 1) {
-            try {
-              await updateBoard({
-                _id: modal.board.id,
-                title: values.name,
-                owner: values.owner,
-                description: values.description,
-                color: values.color,
-                users: [],
-              }).unwrap();
-              dispatch(setModalState(false));
-            } catch (error) {
-              console.error('rejected', error);
-            }
-          } else {
-            try {
-              await createBoard({
-                title: values.name,
-                owner: values.owner,
-                description: values.description,
-                color: values.color ? values.color : 'rgb(140, 140, 140)',
-                users: [],
-              }).unwrap();
-              dispatch(setModalState(false));
-            } catch (error) {
-              console.error('rejected', error);
-            }
-          }
-        })}
-      >
-        <TextInput
-          classNames={inputClasses}
-          label="Name"
-          placeholder="Name"
-          {...form.getInputProps('name')}
-        />
-        <Select
-          searchable
-          classNames={inputClasses}
-          label="Owner"
-          placeholder="Select user"
-          data={usersList}
-          {...form.getInputProps('owner')}
-        />
-        <ColorInput
-          classNames={inputClasses}
-          label="Color"
-          format="rgb"
-          placeholder="Select color"
-          {...form.getInputProps('color')}
-        />
-        <Textarea
-          classNames={inputClasses}
-          label="Description"
-          placeholder="Description"
-          {...form.getInputProps('description')}
-        />
-        <Button className={cl.submit} type="submit" mt="sm">
-          {modal.type === 1 ? 'Save' : 'Create'}
-        </Button>
-      </form>
+      {isLoading ? <Loader color="dark" /> : formComponent}
     </Modal>
   );
 };
