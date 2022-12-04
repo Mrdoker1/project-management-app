da37b217f27f0de50d2a7c470d0a610a1014e7e9
Vlad <makrak.vladislav@yandex.ru>
2022-12-04 03:14:56 +0300

    refactor: refactor for better readability

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,48 +1,48 @@
-import {
-  Avatar,
-  Drawer,
-  Flex,
-  Title,
-  Text,
-  Group,
-  Button,
-  Loader,
-  Accordion,
-  TextInput,
-} from '@mantine/core';
-import React, { memo, useCallback } from 'react';
+import { Avatar, Drawer, Flex, Title, Text, Group, Button, TextInput } from '@mantine/core';
+import React, { memo, useCallback, useState } from 'react';
 import { useAppDispatch, useAppSelector } from 'hooks/redux';
 import {
   setProfileMenuState,
   setProfileEditState,
   setProfileBoardsSearch,
 } from 'store/profileMenuSlice';
-import cl from './ProfilePage.module.css';
 import ProfileModal from './ProfileModal/ProfileModal';
 import { useGetBoardsByUserIdQuery } from 'store/api/boards';
 import { useGetTasksSetQuery } from 'store/api/tasks';
 import { t } from 'i18next';
 import { IconSearch } from '@tabler/icons';
 import { useNavigate } from 'react-router-dom';
-import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
+import { openConfirmModal } from '@mantine/modals';
+import { IconX, IconCheck } from '@tabler/icons';
+import { showNotification } from '@mantine/notifications';
+import { setToken } from 'store/authSlice';
+import { api } from 'store/api';
+import { clearProfile } from 'store/profileSlice';
+import { useDeleteUserMutation } from 'store/api/users';
+import cl from './ProfilePage.module.css';
+import ProfileTasks from './ProfileTasks/ProfileTasks';
+
+interface IError {
+  data: {
+    statusCode: number;
+    message: string;
+  };
+}
 
 const ProfilePage = memo(() => {
   const dispatch = useAppDispatch();
+  const [isDeleted, setIsDeleted] = useState(false);
   const isOpened = useAppSelector((state) => state.profileMenu.profileIsOpened);
   const { _id, name, avatar } = useAppSelector((state) => state.profile);
   const { data: boards } = useGetBoardsByUserIdQuery(_id!);
-  const { data: tasks, isLoading } = useGetTasksSetQuery({
+  const { data: tasks } = useGetTasksSetQuery({
     ids: [],
     userId: _id!,
     searchQuery: '',
   });
+  const [deleteUser] = useDeleteUserMutation();
   const search = useAppSelector((state) => state.profileMenu.profileBoardSearch);
   const navigate = useNavigate();
-
-  const taskClickHandler = useCallback((route: string) => {
-    dispatch(setProfileMenuState(false));
-    navigate(route);
-  }, []);
 
   const closeMenuHandler = useCallback(() => {
     dispatch(setProfileMenuState(false));
@@ -53,9 +53,56 @@ const ProfilePage = memo(() => {
     dispatch(setProfileEditState(true));
   }, []);
 
-  console.log(tasks, boards);
-
   if (!tasks || !boards) return <div>{t('Ничего не найдено!')}</div>;
+
+  const deleteProfileHandler = () => {
+    openConfirmModal({
+      title: t('Delete Profile'),
+      modalId: 'deleteProfile',
+      centered: true,
+      className: cl.deleteModal,
+      children: <Text size="sm">{t('Are you sure you want to delete your profile?')}</Text>,
+      labels: { confirm: t('Delete'), cancel: t('Cancel') },
+      confirmProps: { color: 'red' },
+      onConfirm: async () => {
+        try {
+          setIsDeleted(true);
+          await deleteUser(_id!);
+          dispatch(setToken(''));
+          dispatch(clearProfile);
+          dispatch(api.util.resetApiState());
+          dispatch(setProfileMenuState(false));
+          dispatch(setProfileEditState(false));
+          navigate('/');
+          showNotification({
+            title: 'Success',
+            message: 'Profile delete',
+            color: 'teal',
+            icon: <IconCheck size={18} />,
+            styles: () => ({
+              root: { backgroundColor: '#101113', border: '1px solid #343A40' },
+              title: { color: '#fff' },
+              description: { color: '#fff' },
+            }),
+          });
+        } catch (err) {
+          const error = err as IError;
+          showNotification({
+            title: `Error ${error.data.statusCode}`,
+            message: `${error.data.message}`,
+            color: 'red',
+            icon: <IconX size={18} />,
+            styles: () => ({
+              root: { backgroundColor: '#101113', border: '1px solid #343A40' },
+              title: { color: '#fff' },
+              description: { color: '#fff' },
+            }),
+          });
+        }
+        setIsDeleted(false);
+      },
+    });
+  };
 
   return (
     <>
@@ -65,11 +112,7 @@ const ProfilePage = memo(() => {
         className="profile-drawer"
         zIndex={100}
         position="right"
-        styles={() => ({
-          closeButton: {
-            color: '#fff',
-          },
-        })}
+        withFocusReturn={true}
       >
         <div className="animated-background animated-background--centerRight"></div>
 
@@ -96,8 +139,16 @@ const ProfilePage = memo(() => {
           >
             {t('Edit Profile')}
           </Button>
-          <Button className={cl.button} variant="subtle" radius={8} sx={{ height: 45 }}>
-            {t('Change Avatar')}
+          <Button
+            className={cl.button}
+            variant="subtle"
+            radius={8}
+            loaderPosition="center"
+            loading={isDeleted}
+            sx={{ height: 45 }}
+            onClick={deleteProfileHandler}
+          >
+            {t('Delete Profile')}
           </Button>
         </Group>
         <Text align="center" size={18} color="#909296">
@@ -119,60 +170,17 @@ const ProfilePage = memo(() => {
             }}
           />
         )}
-        <OverlayScrollbarsComponent
-          defer
-          options={{
-            overflow: {
-              y: 'scroll',
-              x: 'hidden',
-            },
-          }}
-        >
-          <>
-            <Accordion className={cl.boardWrapper}>
-              {isLoading ? (
-                <Loader color="dark" />
-              ) : (
-                boards.map((board, index) => {
-                  return (
-                    <Accordion.Item key={index} value={board.title} className={cl.boardItem}>
-                      <Accordion.Control className={cl.boardTitle}>{board.title}</Accordion.Control>
-                      {tasks.map((task, index) => {
-                        if (
-                          task.description.toLowerCase().includes(search.toLowerCase()) ||
-                          task.title.toLowerCase().includes(search.toLowerCase())
-                        ) {
-                          if (board._id === task.boardId) {
-                            return (
-                              <Accordion.Panel
-                                key={index}
-                                className={cl.boardDescription}
-                                onClick={() => taskClickHandler(`/projects/${board._id}`)}
-                              >
-                                <Text size={16} color="#fff">
-                                  {task.title}
-                                </Text>
-                                <Text size={14} color="#868E96">
-                                  {task.description}
-                                </Text>
-                              </Accordion.Panel>
-                            );
-                          }
-                        }
-                      })}
-                    </Accordion.Item>
-                  );
-                })
-              )}
-            </Accordion>
-          </>
-        </OverlayScrollbarsComponent>
+        <ProfileTasks />
       </Drawer>
       <ProfileModal />
     </>
   );
 });
 
-const searchClasses = { input: cl.searchInput, root: cl.searchInput, label: cl.searchInput };
+const searchClasses = {
+  input: cl.searchInput,
+  root: cl.searchWrapper,
+  label: cl.searchInput,
+};
 
 export default ProfilePage;
