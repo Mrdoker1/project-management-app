e7a1f5332f7e84704e9399dd42f4bff40f741346
Viachaslau Kulbitski <fixrapdok@gmail.com>
2023-03-05 10:49:14 +0300

    update: connection string

--- src/store/api.ts
+++ src/store/api.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://final-task-backend-production-3625.up.railway.app',
+    baseUrl: 'https://final-task-backend-production-3ebe.up.railway.app',
     prepareHeaders: (headers, { getState }) => {
       headers.set('Content-type', 'application/json');
       const token = (getState() as RootState).auth.token;
